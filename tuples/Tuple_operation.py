47a2e6f1200a39da7e45340116d0de1dc9b6e46c
ilma-gulzar21 <ilmagulzar2@gmail.com>
2026-08-16 00:55:19 +0530

    Updated files"

---
+++ tuples/Tuple_operation.py
@@ -0,0 +1,8 @@
+marks=(6,3,5,7,8,1,9,0,7)  #immutable
+print(type(marks))
+print(marks)
+
+
+print(marks.index(6))    #find Element at index 6
+print(marks.count(7))   #count occurance of 7 element
+
