f0c732c44bccce56595c7f9fa3b4728ebcf27d0d
ilma-gulzar21 <ilmagulzar2@gmail.com>
2026-08-15 23:07:59 +0530

    Added new program

---
+++ Basics/find_Decimal.py
@@ -0,0 +1,9 @@
+num = float(input("Enter a Floating Value = "))
+
+integer = int(num)
+floating = num - integer
+floating = round(num - integer, 2)
+
+print("integer Part = ",integer)
+print("Floating Part = ",floating)
+
