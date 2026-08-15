7807b13572f15fdc209203c4b8b249d10ffaa622
ilma-gulzar21 <ilmagulzar2@gmail.com>
2026-08-16 00:51:58 +0530

    Added maximum element program

--- list/max_El.py
+++ list/max_El.py
@@ -1,8 +1,19 @@
 marks = [28,89,53,93,30,52,12]
 max=0
+
+#using index value
 n = range(len(marks))
 for i in n:
     if max<marks[i]:
          max=marks[i]
-
 print("maximum Element = {}".format(max))
+
+
+
+#using direct value
+max1=0
+for i in marks:
+    if max1<i:
+         max1=i
+
+print("maximum Element = {}".format(max1))
